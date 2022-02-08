64a7d06fee1ae0c2a8945537d99969d02ded3191
Steeely Dan <steeelydan@posteo.de>
2022-02-08 20:17:47 +0100

    Add jest options for importing built modules in tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 export default {
     preset: 'ts-jest/presets/default-esm',
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1'
+    },
     globals: {
         'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.json'
+            tsconfig: '<rootDir>/tsconfig.json',
+            useESM: true
         }
     },
     modulePathIgnorePatterns: ['build']
